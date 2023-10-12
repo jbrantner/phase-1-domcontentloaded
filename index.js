fd7ba20168bcb75b2b95253d1a6baaaabb54c112
Jordan Brantner <thejordanbrantner@gmail.com>
2023-10-12 15:46:42 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let el = document.getElementById("text")
+    const text = "This is really cool!"
+    el.textContent = text
+})
